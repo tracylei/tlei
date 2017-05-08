de6d388ba2eaa036aab28a2ae3dfcfc702162f8e
Tracy Lei <tracylei.yq@gmail.com>
2017-05-08 00:45:54 -0400

    Add basic burger menu

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
+import { elastic as Menu } from 'react-burger-menu'
 
 class App extends React.Component {
-  render () {
-    return <p> Hello!</p>;
-  }
+    showSettings(event) {
+        event.preventDefault();
+    }
+
+    getMenu(){
+        var menuStateChange = function(state) {
+            // Remove static sidebar when menu is open
+            // Add it back when menu is closed
+            return state.isOpen;
+        };
+
+        return (
+            <Menu 
+                pageWrapId={"page-wrap"} 
+                outerContainerId={"outer-container"}
+                onStateChange={menuStateChange}
+                customBurgerIcon={ <img src="img/burger.svg" /> }>
+                    <a id="home" className="menu-item" href="/">Home</a>
+                    <a id="about" className="menu-item" href="/about">About</a>
+                    <a id="contact" className="menu-item" href="/contact">Contact</a>
+                    <a onClick={this.showSettings} className="menu-item--small" href="">Settings</a>
+            </Menu>
+        );
+    }
+
+    render() {
+        return (
+            <div id="outer-container" style={{height: '100%'}}>
+                {this.getMenu()}
+                <main id="page-wrap">
+                    <h1> "Hello" </h1>
+                </main>
+            </div>
+        );
+    }
 }
 
 export default App;
