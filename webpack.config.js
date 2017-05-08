9f7a25fc95c0328f2488f4a09bafa54100d027b8
Tracy Lei <tracylei.yq@gmail.com>
2017-05-08 02:12:55 -0400

    Load css

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 var HTMLWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 var HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
     template: __dirname + '/app/index.html',
     filename: 'index.html',
     inject: 'body'
 });
+var ExtractTextPluginConfig = new ExtractTextPlugin("[name].css");
 
 module.exports = { 
     entry: __dirname + '/app/index.jsx',
@@ -13,6 +16,10 @@ module.exports = {
                 test: /\.jsx$/,
                 exclude: /node_modules/,
                 loader: 'babel-loader'
+            },
+            {   
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' })
             }
         ]
     },
@@ -20,5 +27,5 @@ module.exports = {
         filename: 'bundle.js',
         path: __dirname + '/build'
     },
-    plugins: [HTMLWebpackPluginConfig]
+    plugins: [HTMLWebpackPluginConfig, ExtractTextPluginConfig]
 };
